2ff3d55c316efa2baf7a5210b4a8d16b0c306d7f
dj 2828 <dj28partystudio@gmail.com>
2026-02-12 15:33:01 +0100

    Update 4.cpp

--- 3C1/info/l32/4.cpp
+++ 3C1/info/l32/4.cpp
@@ -33,13 +33,53 @@ int main(){
 
     int r, c;
     for(int i=0; i<numero_torpediniere; i++){
-        r=rand()%n;
-        c=rand()%n;
-        for(int j=r; j<r+2; j++){
-            for(int k=r; k<c+2; k++){
-
+        bool ripeti=false;
+        do{
+            ripeti=false;
+            r=rand()%n;
+            c=rand()%n;
+            for(int j=c; j<c+2; j++){
+                if(Mat[r][j]!='*') ripeti=true;
             }
+        }while(ripeti);
+        for(int j=c; j<c+2; j++){
+            Mat[r][j]='T';
         }
+    }
+    for(int i=0; i<numero_sottomarini; i++){
+        bool ripeti=false;
+        do{
+            ripeti=false;
+            r=rand()%n;
+            c=rand()%n;
+            for(int j=c; j<c+3; j++){
+                if(Mat[r][j]!='*') ripeti=true;
+            }
+        }while(ripeti);
+        for(int j=c; j<c+3; j++){
+            Mat[r][j]='S';
+        }
+    }
+    for(int i=0; i<numero_corazzate; i++){
+        bool ripeti=false;
+        do{
+            ripeti=false;
+            r=rand()%n;
+            c=rand()%n;
+            for(int j=c; j<c+4; j++){
+                if(Mat[r][j]!='*') ripeti=true;
+            }
+        }while(ripeti);
+        for(int j=c; j<c+4; j++){
+            Mat[r][j]='C';
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << Mat[i][j] << " ";
+        }
+        cout << endl;
     }
 
     system("pause");

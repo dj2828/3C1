204898a73d79479f4cbef955bdbe8d3f419cb696
dj 2828 <dj28partystudio@gmail.com>
2026-02-20 20:21:59 +0100

    scende :)

--- 3C1/info/l32/tetriz.cpp
+++ 3C1/info/l32/tetriz.cpp
@@ -4,7 +4,7 @@
 #include <conio.h>
 using namespace std;
 
-const int r=10, c=5;
+const int r=15, c=10;
 
 char Mat[r][c];
 
@@ -14,16 +14,46 @@ struct blocco_base{
         riga = r;
         colonna = c;
     }
-    void scendi(int direzione=0){
-        riga++;
-        if(direzione==1) colonna++;
-        if(direzione==2) colonna--;
+    int posso_spostarmi(int x){
+        
+        return 1;
+    }
+    void sposta(int direzione=0){
+        if(direzione==1 && colonna<c-1) colonna++; // destra
+        if(direzione==2 && colonna>0) colonna--; // sinistra
+    }
+    void scendi(){
+        if(riga<=r-1 && Mat[riga][colonna] != '-') riga++;
     }
 
 };
 blocco_base blocco;
 
+
+void genBlocco(){
+    blocco = blocco_base();
+}
+
+void check_in_fondo(){
+    if(!(blocco.riga<=r-1 && Mat[blocco.riga][blocco.colonna] != '-')){
+        Mat[(Mat[blocco.riga][blocco.colonna] == '-') ? blocco.riga-1 : r-1][blocco.colonna] = '-';
+        genBlocco();
+    }
+}
+
+void aggiorna_mat(){
+    check_in_fondo();
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            if(Mat[i][j] == '-') Mat[i][j] = '-';
+            else Mat[i][j] = (blocco.riga == i && blocco.colonna == j) ? 'X' : '*';
+        }
+    }
+}
+
+
 void disp(){
+    aggiorna_mat();
     system("cls");
     for(int i=0; i<r; i++){
         for(int j=0; j<c; j++){
@@ -41,22 +71,12 @@ void genMat(){
     }
 }
 
-void genBlocco(){
-    blocco = blocco_base();
-}
-
 void sleep(int x){
-    for (int i = 0; i < x*1000; i += 10) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-        if (_kbhit()) {
-            char c = _getch();
-            std::cout << "Premuto: " << c << "\n";
-        }
-    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(x*1000));
 }
-int input(){
-    for (int i = 0; i < x*1000; i += 10) {
+int input(int refresh_rate = 250){
+    blocco.scendi();
+    for (int i = 0; i < refresh_rate; i += 10) {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
         if (_kbhit()) {
@@ -66,26 +86,23 @@ int input(){
                 ch = _getch();
 
                 switch (ch) {
-                    case 72: std::cout << "Freccia SU\n"; break;
-                    case 80: std::cout << "Freccia GIU\n"; break;
-                    case 75: std::cout << "Freccia SINISTRA\n"; break;
-                    case 77: std::cout << "Freccia DESTRA\n"; break;
+                    case 80: std::cout << "Freccia GIU\n"; blocco.scendi(); break;
+                    case 75: std::cout << "Freccia SINISTRA\n"; blocco.sposta(2); break;
+                    case 77: std::cout << "Freccia DESTRA\n"; blocco.sposta(1); break;
                 }
+                disp();
             }
         }
     }
-
     return 0;
 }
 
 int main(){
     genMat();
     while(true){
-        cout << blocco.riga << " " << blocco.colonna << endl;
-        sleep(1);
-        blocco.scendi(input());
+        disp();
+        input();
     }
-
     system("pause");
     return 0;
 }

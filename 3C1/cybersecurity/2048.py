1420b24aa0d1e01bd49b9ff2013f842c0ca00b48
dj 2828 <dj28partystudio@gmail.com>
2026-03-30 16:19:23 +0200

    fix

--- 3C1/cybersecurity/2048.py
+++ 3C1/cybersecurity/2048.py
@@ -4,28 +4,25 @@ url = "2048.challs.olicyber.it"
 port = 10007
 conn = pwn.remote(url, port)
 
-i=0
-while i!=2048:
-    bit = conn.recv()
+for i in range(2048):
+    bit = conn.recv().decode()
 
-    ultima_riga = bit.split(b"\n")[-1]
-    print(f"Ultima riga: {ultima_riga} - Iterazione: {i}")
-    operazione, n1, n2 = ultima_riga.strip().split(b" ")
+    ultima_riga = bit.split("\n")[-1]
+    operazione, n1, n2 = ultima_riga.strip().split(" ")
 
-    print(f"Operazione: {operazione}, n1: {n1}, n2: {n2}")
+    print(f"Operazione {i}: {operazione}, n1: {n1}, n2: {n2}")
 
-    if operazione == b"SOMMA":
+    if operazione == "SOMMA":
         risultato = int(n1) + int(n2)
-    elif operazione == b"DIFFERENZA":
+    elif operazione == "DIFFERENZA":
         risultato = int(n1) - int(n2)
-    elif operazione == b"PRODOTTO":
+    elif operazione == "PRODOTTO":
         risultato = int(n1) * int(n2)
-    elif operazione == b"DIVISIONE_INTERA":
+    elif operazione == "DIVISIONE_INTERA":
         risultato = int(n1) // int(n2)
-    elif operazione == b"POTENZA":
+    elif operazione == "POTENZA":
         risultato = int(n1) ** int(n2)
 
-    conn.sendline(str(risultato))
-    i+=1
+    conn.sendline(str(risultato).encode())
 
-print(conn.recv())
+print(conn.recv().decode())
